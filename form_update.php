a9da83abd0e0dd4e16c499c648d8ee434e86f5ca
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-03-03 11:22:55 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -14,8 +14,8 @@
       <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
-
-	  <div style="margin: auto; width:15%; border:3px solid black;padding: 10px;">
+<div class="navbar">
+	
 	    <ul>
        <li> <a href="index.php?navigation=product">Show</a> </li>
          <li><a href="index.php?navigation=categories">Category</a> </li>
@@ -25,6 +25,7 @@
     </div>
 	<h1> Update Product </h1>
 <form action="pro_update.php?id=<?php echo $id ?>" method="POST"><br><br>
+	  <div style="margin: auto; width:15%; border:3px solid black;padding: 10px;">
 	<input type="text" name="name" value="<?php echo $result['name'];?>"/><br><br>
 	<input type="text" name="description" value="<?php echo $result['description']; ?>"/><br><br>
 	<input type="text" name="price" value="<?php echo $result['price']; ?>"/><br><br>
