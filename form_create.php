a0cb524facb2a93c4c6d44d213406276ffdcd38d
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-03-03 10:43:39 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -8,10 +8,10 @@
 <form action="pro_create.php" method="POST">
 <h1> Create Product </h1>
 	<ul>
-<li><input type="text" name="name" placeholder="name"/></li>
-<li><input type="text" name="description" placeholder="description"/></li>
-<li><input type="text" name="price" placeholder="price"/></li>
-<li><select name="category"></li>
+<li><input type="text" name="name" placeholder="name"/></li><br>
+<li><input type="text" name="description" placeholder="description"/></li><br>
+<li><input type="text" name="price" placeholder="price"/></li><br>
+<li><select name="category"></li><br>
 	</ul>
 <option value="">--Category--</option>
 	<?php
