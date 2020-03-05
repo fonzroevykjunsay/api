aa847aa616b30e7b95ade11bf10bd8b2c41f128a
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-03-05 11:04:02 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -14,15 +14,7 @@
       <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
-<div class="navbar">
-	
-	    <ul>
-       <li> <a href="index.php?navigation=product">Show</a> </li>
-         <li><a href="index.php?navigation=categories">Category</a> </li>
-        <li> <a href="index.php?navigation=create">Create</a> </li>
-       <li>  <a href="index.php?navigation=delete">Delete</a> </li>
-	    </ul>
-    </div>
+
 	<h1> Update Product </h1>
 <form action="pro_update.php?id=<?php echo $id ?>" method="POST"><br><br>
 	  <div style="margin: auto; width:15%; border:3px solid black;padding: 10px;background-color: #deddfa">
